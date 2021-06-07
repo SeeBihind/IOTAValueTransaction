cdda9bce5ad49b1703285377a334dd82c6c086fb
SeeBihind <74867534+Sebihind@users.noreply.github.com>
2021-06-07 21:21:19 +0200

    Test

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	// http://goshimmer.docs.iota.org/tutorials/wallet.html
 
 	goshimAPI := client.NewGoShimmerAPI("http://82.165.69.143:8080")
-	const seed string = "3heE6gdT3aAqRGZ2b7mARTjEQEvuVDACiC3CkDVQ1Nu6"
+	const seed string = "5fjhwKpDe6yBHD4hohJmHLtfHGjq5L55kqhdZRPSHhmr"
 
 	// Faucet
 
@@ -29,7 +29,16 @@ func main() {
 
 	// Generate new address with index
 	myAddr := mySeed.Address(0)
+
+	// My DevNet Seed
+	// HnMtW6DsaPGFb4X11VzM9TFVSsJTmhBSPnxYhfm5f89C
+
+	//seedBytes2, _ := base58.Decode("DkxqNM1r1crSFKhnFvarQa3Te2jijjh29voUdYocW8qU")
+	//devNetByte := walletseed.NewSeed(seedBytes2)
+	//devNetAdresse := devNetByte.Address(0)
+
 	fmt.Println("My Address: ", myAddr.String())
+	//fmt.Println("Zieladresse: ", devNetAdresse.String())
 
 	// Prüft ob Guthaben zur verfügung steht und nicht bestätigt ist
 	resp, _ := goshimAPI.PostAddressUnspentOutputs([]string{myAddr.Base58()}) // ignoring error
@@ -40,18 +49,19 @@ func main() {
 
 	// STEP 2 Transaction essence ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	var version TransactionEssenceVersion
-	var timestamp time.Time
+	//var timestamp time.Time
 	var accessPledgeID identity.ID
 	var consensusPledgeID identity.ID
 	var inputs ledgerstate.Inputs
-	var outputs ledgerstate.Outputs
+	//var outputs ledgerstate.Outputs
+	var version ledgerstate.TransactionEssenceVersion
+	var timestamp time.Time
 
 	// Version und Zeitstempel
 	version = 0
 	timestamp = time.Now()
 
-	fmt.Println("Version:", version, "Zeitstempel:", timestamp)
+	//fmt.Println("Version:", version, "Zeitstempel:", timestamp)
 
 	// Convert NodeID for accessMana /////////////////////////////////////////////////////////////////////////
 	const nodeID string = "j6nX8BELUcQ"
@@ -67,6 +77,41 @@ func main() {
 
 	// Step 3 Inputs ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 	// Bereitstellen von nicht ausgegebenen Ausgaben
+
+	resp2, _ := goshimAPI.GetAddressUnspentOutputs(myAddr.Base58()) // ignoring error
+	// iterate over unspent outputs of an address
+	var out ledgerstate.Output
+	for _, output := range resp2.Outputs {
+		out, _ = output.ToLedgerstateOutput() // ignoring error
+		balance, colorExist := out.Balances().Get(ledgerstate.ColorIOTA)
+		fmt.Println(balance, colorExist)
+	}
+
+	out.Balances().ForEach(func(color ledgerstate.Color, balance uint64) bool {
+		fmt.Println("Color:", color.Base58())
+		fmt.Println("Balance:", balance)
+		return true
+	})
+
+	inputs = ledgerstate.NewInputs(ledgerstate.NewUTXOInput(out.ID()))
+
+	balance := ledgerstate.NewColoredBalances(map[ledgerstate.Color]uint64{
+		ledgerstate.ColorIOTA: uint64(1000000),
+	})
+
+	output := ledgerstate.NewOutputs(ledgerstate.NewSigLockedColoredOutput(balance, myAddr.Address()))
+	kp := *mySeed.KeyPair(0)
+	txEssence := ledgerstate.NewTransactionEssence(version, timestamp, accessPledgeID, consensusPledgeID, inputs, output)
+	signature := ledgerstate.NewED25519Signature(kp.PublicKey, kp.PrivateKey.Sign(txEssence.Bytes()))
+	unlockBlock := ledgerstate.NewSignatureUnlockBlock(signature)
+	tx := ledgerstate.NewTransaction(txEssence, ledgerstate.UnlockBlocks{unlockBlock})
+
+	resp5, err := goshimAPI.PostTransaction(tx.Bytes())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Transaction issued, txID:", resp5.TransactionID)
 
 }
 
